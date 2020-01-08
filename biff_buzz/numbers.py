761425d89fa583365ecd93ba6fefe71a1a0acd1c
apiliuk <lohika.artur@98point6.com>
2020-01-08 16:29:13 +0200

    added tests

---
+++ biff_buzz/numbers.py
@@ -0,0 +1,15 @@
+def test_biff_test():
+    assert biff_buzz(3) == "Biff"
+
+
+def test_buzz_test():
+    assert biff_buzz(5) == "Buzz"
+
+
+def test_biff_buzz_test():
+    assert biff_buzz(15) == "BiffBuzz"
+
+
+def biff_buzz(number):
+    # TODO
+    1 + 1
